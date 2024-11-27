f23e9c2c151ff925203958fbef39213d91f09b68
Tiuters <vacuumdocs1@gmail.com>
2024-11-27 14:43:00 +0300

    Controller and Dockerfile

---
+++ src/main/java/com/example/springbootforjenkins/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.springbootforjenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/jenkins")
+    public String test() {
+        return "<h1>Hello, Andrey!</h>";
+    }
+}
